a27ab3244174bcae353838b46b7061ae9a81cb37
Chad Saglam <saglam.chd@gmail.com>
2020-01-20 12:53:20 +0100

    Create API Router

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const errorhandler = require('errorhandler');
+const apiRouter = require('./api/api');
 const app = express();
 
 const PORT = process.env.PORT || 4000;
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(errorhandler());
 
+app.use('/api', apiRouter);
 
 app.get('/', (req, res, next) => {
    res.send("<h1>Welcome to X-Press Publishing</h1>");
